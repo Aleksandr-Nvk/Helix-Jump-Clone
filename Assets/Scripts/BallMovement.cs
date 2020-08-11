7c1accc8b8239ca48992b5b4567214b1708ad35b
Oduvaanchikk <alexandr.novik2@gmail.com>
2020-08-11 18:35:26 +0300

    Ball jumping movement added

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+#pragma warning disable 0649
+
+    [SerializeField] private Rigidbody ball;
+
+#pragma warning restore
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        ball.velocity = Vector3.zero;
+        ball.AddForce(Vector3.up * 2, ForceMode.Impulse);
+    }
+}
